9a21f8412fff4d7992c1d60a6e21d4cc1d2f2922
Anurag Singh <119996443+theanuragg@users.noreply.github.com>
2024-09-02 22:36:14 +0500

    Update index.tsx

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,10 +1,10 @@
 export { HomeView} from "./home";
 export { ToolView} from "./tools";
-export { FeatureView} from "./feature";
+//export { FeatureView} from "./feature";
 export { OfferView} from "./offer";
 export { FaqView} from "./faq";
-export { AirdropView} from "./airdrop";
-export { DonateView } from "./donate";
+//export { AirdropView} from "./airdrop";
+//export { DonateView } from "./donate";
 export { CreateView } from "./create";
 export { InputView } from "./input";
 export { TokenMetadata } from "./tokenMetadata";
