2265763a8262de422b8b26c111f807e7a26a0da4
Anurag Singh <119996443+theanuragg@users.noreply.github.com>
2024-09-02 22:32:33 +0500

    Update index.tsx

--- src/views/input/index.tsx
+++ src/views/input/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { FC } from "react";
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
+interface InputViewProps {
+  placeholder: string;
+  name: string;
+  Clickhandle: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default index
+export const InputView: FC<InputViewProps> = ({ placeholder, name, Clickhandle }) => {
+  return (
+    <div className="mb-4">
+      <label
+        htmlFor={name.toLowerCase().replace(" ", "-")} // Dynamically setting the htmlFor attribute based on the name
+        className="text-base/normal text-default-200 mb-2 block font-semibold"
+      >
+        {name}
+      </label>
+      <input
+        type="text"
+        id={name.toLowerCase().replace(" ", "-")}
+        onChange={Clickhandle}
+        placeholder={placeholder}
+        className="border-default-200 block w-full rounded border-white/10 bg-transparent py-1.5 px-3 text-white/80 focus:border-white/25 focus:ring-transparent"
+      />
+    </div>
+  );
+};
